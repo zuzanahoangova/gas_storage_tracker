691d8283b1b9a2a3053697065ed26855ac46af86
zuzanahoangova <250833264+zuzanahoangova@users.noreply.github.com>
2026-01-01 17:15:39 +0100

    logic for choosing mode in progress

--- storage_tracker.py
+++ storage_tracker.py
@@ -1,22 +1,45 @@
 import pandas as pd
 import numpy as np
 import os
+import requests
+from datetime import date, timedelta
+
+today = date.today()
+
+
+# pick op mode (summer, winter, transition)
+def season_mode(date_today):
+    m, d = date_today.month, date_today.day
+    if (m == 3 and d == 31) or (4 <= m <= 9) or (m == 10 and d == 1):
+        return 1    # summer mode march 31 - oct 1
+    elif (m == 12 and d >= 2) or (m in [1, 2]) or (m == 3 and d <= 30):
+        return 2    # winter mode dec 2 - march 30
+    else:
+        return 0    # transition oct 2 - dec 1
 
 
 # fetch data from agsi
-def fetch_data(dunno):  # TODO: learn API
+def fetch_data(mode, dunno):  # TODO: learn API
     # TODO: save in current folder as default but implement choose
     return
 
 
 # load data from same folder
 def load_data(filename):
-    data = pd.read_csv(filename, delimiter=";", nrows=14)  # TODO: change file type later, filename as string
+    data = pd.read_json(filename)
     return data
 
 
+# check for missing data
+def check_data(data):   # TODO: do a data check (correct date, no missing days etc.)
+    return
+# give a warning window with missing data and let decide if proceed or not.
+# or maybe just proceed anyways and just list what data is missing?
+
+
 # clean data
-def extract_data(data):  # TODO: do a data check (correct date, no missing days etc.)
+def extract_data(data):
+    data = data.iloc[:14]
     st_spec = pd.DataFrame({
         "gas_day": [data.iloc[0, 2]],               # current day as string
         "tech_capacity": [data.iloc[0, 10]],        # in TWh
@@ -34,7 +57,6 @@ def calculate_summer(st_spec, st_flow):
 
     # policy: hit 90% once between oct 1 and dec 1
     factor = 0.9  # policy goal
-    # TODO: implement 10% wing room later
     nov1 = pd.Timestamp(gas_day.year, 11, 1)  # nov 1: anchor date, old policy 2022-2025
     dec1 = pd.Timestamp(gas_day.year, 12, 1)  # dec 1: hard deadline, new policy after april 2025
     days_to_nov1 = (nov1 - gas_day).days
@@ -46,12 +68,15 @@ def calculate_summer(st_spec, st_flow):
     withdrawal14 = st_flow["withdrawal"].mean()
 
     # all following flows in GWh
-    # calculations assume net injection regime (injection > withdrawal)
+    # calculations assume net injection regime (injection > withdrawal)  # TODO: what if low/no injections?
+
+    # How many days left until policy goal fulfilled?
     days_to_goal7 = (tech_capacity * factor - current_storage) * 1000 / (injection7 - withdrawal7)
     days_to_goal14 = (tech_capacity * factor - current_storage) * 1000 / (injection14 - withdrawal14)
     goal_date7 = gas_day + pd.Timedelta(days=np.ceil(days_to_goal7))
     goal_date14 = gas_day + pd.Timedelta(days=np.ceil(days_to_goal14))
 
+    # What minimal injection rate is needed to reach policy goal based on recent withdrawal rate?
     min_inj_to_nov1_7 = (tech_capacity * factor - current_storage) * 1000 / days_to_nov1 + withdrawal7
     min_inj_to_nov1_14 = (tech_capacity * factor - current_storage) * 1000 / days_to_nov1 + withdrawal14
     min_inj_to_dec1_7 = (tech_capacity * factor - current_storage) * 1000 / days_to_dec1 + withdrawal7
@@ -87,14 +112,24 @@ def calculate_winter(st_spec, st_flow):
     else:
         eow = pd.Timestamp(gas_day.year + 1, 3, 31)
 
-    days_to_eow = (eow - gas_day).days  # TODO: handle zero days_to_eow / zero withdrawal edge cases
+    days_to_eow = (eow - gas_day).days
     max_withdrawal_eow = current_storage / days_to_eow
     withdrawal7 = st_flow["withdrawal"].iloc[:7].mean()
     withdrawal14 = st_flow["withdrawal"].mean()
+
+    # How many days of coverage left assuming no more injections?
     days_of_cover7 = current_storage / withdrawal7
     days_of_cover14 = current_storage / withdrawal14
-    end_date7 = gas_day + pd.Timedelta(days=np.floor(days_of_cover7))
-    end_date14 = gas_day + pd.Timedelta(days=np.floor(days_of_cover14))
+
+    if np.isinf(days_of_cover7):
+        end_date7 = pd.NaT
+    else:
+        end_date7 = gas_day + pd.Timedelta(days=np.floor(days_of_cover7))
+
+    if np.isinf(days_of_cover14):
+        end_date14 = pd.NaT
+    else:
+        end_date14 = gas_day + pd.Timedelta(days=np.floor(days_of_cover14))
 
     # columns = scenarios, rows = concepts
     winter_data = pd.DataFrame(
@@ -106,6 +141,20 @@ def calculate_winter(st_spec, st_flow):
         index=["date", "days", "withdrawal"]
     )
     return winter_data
+
+
+def pick_season(mode, st_spec, st_flow):
+    if mode == 1:
+        return calculate_summer(st_spec, st_flow)
+    elif mode == 2:
+        return calculate_winter(st_spec, st_flow)
+    else:
+        return
+    # TODO: logic for Sept-Dec 1
+    # TODO: summer returns 2 objects, winter 1 -> resolve. do i even have to resolve it tho?
+# run summer calc from march 31? fixed winter from dec 1 to march 30.
+# transition window from mid-sept to dec 1 depends on 90% target.
+# i might need to separate "date categorization" and "transition season determination".
 
 
 def save_excel():
